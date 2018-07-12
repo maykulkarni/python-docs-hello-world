be60ab6c2c435272dfe000dd3f3012be46ab99bf
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2018-07-12 15:01:39 +0530

    n

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World! I am a pro coder'
+  return 'Hello, World! I am a pro coder \n #DevOps'
 
 if __name__ == '__main__':
   app.run()
