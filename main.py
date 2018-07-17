8ae41b25943b878b4d384b0351865a796c10c528
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2018-07-17 10:36:03 +0530

    Added mongo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+  client = MongoClient("localhost", 27017)
   return 'Hello, World! I am a pro coder \n #DevOps'
 
 if __name__ == '__main__':
